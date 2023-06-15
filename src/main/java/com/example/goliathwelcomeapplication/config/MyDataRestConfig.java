590d83478226650b3ba7ef0319aa6b2f18d21568
MS <polibudamichal@gmail.com>
2023-06-15 11:22:14 +0200

    Initial Data Rest config created

---
+++ src/main/java/com/example/goliathwelcomeapplication/config/MyDataRestConfig.java
@@ -0,0 +1,46 @@
+package com.example.goliathwelcomeapplication.config;
+
+import com.example.goliathwelcomeapplication.entity.Crew;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String allowedOrrigins = "http://localhost:3000";;
+
+
+
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+
+        HttpMethod[] unSupportedMethods = {
+                HttpMethod.POST,
+                HttpMethod.PUT,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE
+        };
+
+
+        config.exposeIdsFor(Crew.class);
+
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrrigins);
+
+        disableHtppMethods(Crew.class,config,unSupportedMethods);
+    }
+
+    private void disableHtppMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unSupportedMethods){
+
+        config.getExposureConfiguration().forDomainType(Crew.class)
+                .withItemExposure(((metdata, httpMethods) ->
+                        httpMethods.disable(unSupportedMethods)))
+                .withCollectionExposure(((metdata, httpMethods) ->
+                        httpMethods.disable(unSupportedMethods)));
+    }
+
+}
