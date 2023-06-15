ac3f2d36cf1400a4d132359a46c780092a5de011
MS <polibudamichal@gmail.com>
2023-06-15 19:21:24 +0200

    Changed exposed name from crews to crew

--- src/main/java/com/example/goliathwelcomeapplication/dao/CrewRepository.java
+++ src/main/java/com/example/goliathwelcomeapplication/dao/CrewRepository.java
@@ -2,6 +2,8 @@ package com.example.goliathwelcomeapplication.dao;
 
 import com.example.goliathwelcomeapplication.entity.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "crew", path = "crew")
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 }
