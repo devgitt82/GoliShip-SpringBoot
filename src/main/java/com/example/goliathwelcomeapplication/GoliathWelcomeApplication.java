ecbd0b18f4adec50856e3ff79c509e7c48c00e03
MS <polibudamichal@gmail.com>
2023-06-15 10:47:13 +0200

    Initial commit - base app structure created.

---
+++ src/main/java/com/example/goliathwelcomeapplication/GoliathWelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.goliathwelcomeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoliathWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoliathWelcomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goliathwelcomeapplication/GoliathWelcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goliathwelcomeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoliathWelcomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
