61966bab0411b76ec483c5feed83c36197a96595
MS <polibudamichal@gmail.com>
2023-06-16 13:46:30 +0200

    Add findByRankContaining and findByDepartment to CrewRepository

--- src/main/java/com/example/goliathwelcomeapplication/dao/CrewRepository.java
+++ src/main/java/com/example/goliathwelcomeapplication/dao/CrewRepository.java
@@ -1,9 +1,16 @@
 package com.example.goliathwelcomeapplication.dao;
 
 import com.example.goliathwelcomeapplication.entity.Crew;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(collectionResourceRel = "crew", path = "crew")
 public interface CrewRepository extends JpaRepository<Crew, Long> {
+
+    Page<Crew> findByRankContaining(@RequestParam("rank") String rank, Pageable pageable);
+
+    Page<Crew> findByDepartment(@RequestParam("department") String department, Pageable pageable);
 }
