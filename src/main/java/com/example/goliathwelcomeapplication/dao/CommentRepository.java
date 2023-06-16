f6324b225055b48ad46dd3daf87814960c95102b
MS <polibudamichal@gmail.com>
2023-06-16 18:04:59 +0200

    Add CommentRepository

---
+++ src/main/java/com/example/goliathwelcomeapplication/dao/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.goliathwelcomeapplication.dao;
+
+import com.example.goliathwelcomeapplication.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByCrewId(@RequestParam("crew_id") Long crewId, Pageable pageable);
+
+}
